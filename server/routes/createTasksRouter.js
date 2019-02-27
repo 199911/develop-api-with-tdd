c4a9c1abce11838d39e1d1124feacc9716be5164
Ku Lok Sun <sundaykuloksun@gmail.com>
2019-02-27 21:53:15 +0800

    feat: make POST /tasks to pass the test, but feature not finished

--- server/routes/createTasksRouter.js
+++ server/routes/createTasksRouter.js
@@ -4,7 +4,14 @@ const createHealthCheckRouter = (Task) => {
   const router = new Router();
 
   router
-    .route('/tasks');
+    .route('/tasks')
+    .post((req, res) => {
+      const { body } = req;
+      // TODO: Save the task to db
+      res
+        .status(201)
+        .send(body);
+    });
 
   return router;
 }
